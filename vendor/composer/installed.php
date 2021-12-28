9056f12c20114e23696570374041ad9aefa35363
frank1604 <frank.atencio.loreth@gmail.com>
2021-12-28 10:45:28 -0500

    composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '40bc65c3baabf6e0241f2a51a98554bd05563876',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '40bc65c3baabf6e0241f2a51a98554bd05563876',
+            'dev_requirement' => false,
+        ),
+    ),
+);
